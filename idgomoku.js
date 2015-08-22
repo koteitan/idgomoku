8400fc0cd131123cfa8cbf4153ee58c524e82b0b
koteitan <ukaibutton@mac.com>
2015-08-23 05:43:01 +0900

    structure of map stoned[p][s][dt] and stonel[p][s][dt] are replaced into stonelist[p][s][d].

--- idgomoku.js
+++ idgomoku.js
@@ -2,20 +2,29 @@ window.onload=function(){ //entry point
   initGame();
 };
 //fields for game ---------------------------
+var debug=1;
 var turn = 0;
 var turnstr=["black","white"];
 var maxdim = 1;
-var stoned=[]; //stoned[p][s]=demension of s th stone of player p.
-var stonel=[]; //stonel[p][s]=location of s th stone of player p in stoned[p][s] dimension.
+var stonelist=[]; //stoned[p][s][d]=location of s th stone of player p in dim d.
 //initialize game----------------------------
 var initGame=function(){
   document.getElementById("console").innerHTML = "";
   putOut("The new game was initialized.<br>");  
-  stoned=[[[0]],[[]]];
-  stonel=[[[0]],[[]]];
-  putOut(turnstr[0]+"1> 0 0<br>");
+  stonelist=[
+    [//p=0
+      [0]//s=0
+    ],
+    [//p=1
+    ]
+  ];
+  
+  putOut(turnstr[0]+"1> (0)<br>");
   turn=1;
-  putOut(turnstr[turn]+(stoned[turn].length+1)+"> ");
+  putOut(turnstr[turn]+(stonelist[turn].length+1)+"> ");
+  if(debug){
+    putDebug("stonelist="+stonelist.toString()+"<br>");
+  }
 };
 //when push send button ---------------------
 var receiveCommand=function(str){
@@ -34,75 +43,76 @@ var receiveCommand=function(str){
     putOut("error.");
     return "";
   }
-  var dts = a.length/2;
+  var dims = a.length;
   
   //new stone
-  var newstoned=new Array(dts); // dimension of new stone
-  var newstonel=new Array(dts); // location  of new stone
-  for(var dt=0;dt<dts;dt++){
-    if(!isFinite(a[dt/2+0])){
-      putOut("\""+a[dt/2+0]+"\""+"is not a number.<br>");
+  var newstone=new Array(dims); // newstone[d]=location of the new stone
+  for(var d=0;d<dims;d++){
+    if(!isFinite(a[d])){
+      putOut("\""+a[d]+"\""+"is not a number.<br>");
       return form1.command.value;
     }
-    if(!isFinite(a[dt/2+1])){
-      putOut("\""+a[dt/2+1]+"\""+"is not a number.<br>");
-      return form1.command.value;
-    }
-    newstoned[dt]=parseInt(a[dt/2+0]);
-    newstonel[dt]=parseInt(a[dt/2+1]);
+    newstone[d]=parseInt(a[d]);
   }// for dt
 
   //check onto
-  var onto = false;
-  for(var p=0;p<2;p++){
-    for(var s=0;s<stoned[p].length;s++){
-      var sdt_onto=true;
-      for(var ndt=0;ndt<newstoned.length;ndt++){
-        if(!sdt_onto) break;
-        //search and set sdt in which ndt==sdt into sdt_equal (set 0 if not found)
-        var sdt_equal=0;
-        for(var sdt=0;sdt<stoned[p][s].length;sdt++){
-          if(newstoned[ndt] == stoned[p][s][sdt]){
-            if(newstonel[ndt] != stonel[p][s][sdt]){
-              sdt_equal = sdt;
-              break;
-            }
-          }//if
-        }// for sdt
-        if(newstoned[ndt] != stoned[p][s][sdt]){
-          sdt_onto=false;
-          break;
-        }
-      }//for ndt
-      if(sdt_onto){
-        onto=true;
-        break;
-      }
-      if(onto) break;
-    }//for s
-    if(onto) break;
-  }// for p
-  if(onto){
+  if(getStone(newstone)!=-1){
     putOut("[error] already put.<br>");
+    putOut(turnstr[turn]+(stonelist[turn].length+1)+"> ");
     return form1.command.value;
   }
   
   //put
-  stoned[turn].push(newstoned);
-  stonel[turn].push(newstonel);
-
+  stonelist[turn].push(newstone);
+  if(maxdim<newstone.length) maxdim=newstone.length;
   //out
-  for(var dt=0;dt<newstoned.length;dt++){
-    putOut(newstoned[dt]+" "+newstonel[dt]+" ");
+  putOut("(");
+  for(var d=0;d<newstone.length;d++){
+    putOut(newstone[d]);
+    if(d!=maxdim-1) putOut(",");
   }
-  putOut("<br>");
+  putOut(")<br>");
   turn=(turn+1) % 2;
-  putOut(turnstr[turn]+(stoned[turn].length+1)+"> ");
+  putOut(turnstr[turn]+(stonelist[turn].length+1)+"> ");
+
+  //debugout
+  document.getElementById("debugout").innerHTML="";
+  if(debug){
+    putDebug("stonelist="+stonelist.toString()+"<br>");
+  }
   return form1.command.value;
 };
 var putOut=function(str){
   document.getElementById("console").innerHTML += str;
 }
+var putDebug=function(str){
+  document.getElementById("debugout").innerHTML += str;
+}
 var getIn=function(){
   return form1.command.value;
 }
+/* getStone(place) returns the stone in the place.
+  place[d]=location of target place in dimenstion d
+  getStone(place)= 0:black
+  getStone(place)= 1:white
+  getStone(place)=-1:blank
+*/
+getStone=function(place){
+  for(var p=0;p<2;p++){
+    for(var s=0;s<stonelist[p].length;s++){
+      var isfound=true;
+      for(var d=0;d<place.length;d++){
+        var l=0;
+        if(d<maxdim) l=place[d];
+        if(l!=stonelist[p][s][d]){
+          isfound=false;
+          break;
+        }
+      }
+      if(isfound) return p;
+    }
+  }// for p
+  return -1;
+}
+
+
